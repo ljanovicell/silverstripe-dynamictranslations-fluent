25bcf91aa3be2e8d4e2e4436a0763e0369e1f041
Simon M <mail@smdev.dk>
2016-04-22 00:36:34 +0200

    Move _dt() to correct _config.php

--- _config.php
+++ _config.php
@@ -6,3 +6,10 @@
 
 // Dynamic translations should never time-out
 SS_Cache::set_cache_lifetime('DynamicTranslations', null, 100);
+
+// Alias for DynamicTranslationProvider::t()
+function _dt($entity, $string = '', $injectionArray = array()){
+
+  return DynamicTranslationProvider::t($entity,$string,$injectionArray);
+
+}
