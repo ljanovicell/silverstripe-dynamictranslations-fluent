74d79b20bc878fc5d7959f00c67dccd549e67779
Simon M <mail@smdev.dk>
2016-04-22 00:41:27 +0200

    Cast to string in _dt()

--- _config.php
+++ _config.php
@@ -10,6 +10,6 @@ SS_Cache::set_cache_lifetime('DynamicTranslations', null, 100);
 // Alias for DynamicTranslationProvider::t()
 function _dt($entity, $string = '', $injectionArray = array()){
 
-  return DynamicTranslationProvider::t($entity,$string,$injectionArray);
+  return (string) DynamicTranslationProvider::t($entity,$string,$injectionArray);
 
 }
